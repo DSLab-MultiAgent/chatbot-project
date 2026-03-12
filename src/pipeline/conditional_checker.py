4e174399fbb93731a0b21ce1e6a567651396df00
SeungKeon Lee / 이승건 <113653735+seungdeng@users.noreply.github.com>
2026-03-12 17:16:29 +0900

    code fix
    conditional_checker 호출 -> NULL값 반환
    
    conditional_check으로 호출 변수명 수정

--- src/pipeline/conditional_checker.py
+++ src/pipeline/conditional_checker.py
@@ -14,7 +14,7 @@ class ConditionalChecker:
     
     def __init__(self):
         self.llm_client = LLMClient()
-        self.prompt_template = yaml_config.get('prompts', {}).get('conditional_checker', '')
+        self.prompt_template = yaml_config.get('prompts', {}).get('conditional_check', '')
     
     async def check_conditional(
         self, 
